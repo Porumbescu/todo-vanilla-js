980e604363b18f32d658fdaa771eb61965f580c7
Porumb Mihai-Cristian <cristianporumb3@gmail.com>
2023-10-17 18:46:21 +0300

    basic structure

---
+++ App/main.js
@@ -0,0 +1,41 @@
+let users = [
+    {
+        id: 1,
+        name: "Cristian",
+        email: "cristianporumb3@gmail.com",
+        password: "Cristian1!"
+    },
+    {
+        id: 2,
+        name: "Mihai",
+        email: "mihaiporumb3@gmail.com",
+        password: "Mihai1!"
+    }
+]
+
+let toDos = [
+    {
+        id: 1,
+        taskName: "do the dishes",
+        status: "to-do",
+        responsible: "Cristian"
+    },
+    {
+        id: 2,
+        taskName: "do the laundry",
+        status: "to-do",
+        responsible: "Cristian"
+    },
+    {
+        id: 3,
+        taskName: "do your homework",
+        status: "in progress",
+        responsible: "Mihai"
+    },
+    {
+        id: 4,
+        taskName: "binging",
+        status: "done",
+        responsible: "Mihai"
+    },
+]
